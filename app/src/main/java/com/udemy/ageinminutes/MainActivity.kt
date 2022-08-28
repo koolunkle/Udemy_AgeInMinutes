681367300c26c236438d9172dcbbc83cc2fdd247
김준기 <gugi329@daum.net>
2022-08-28 14:49:53 +0900

    분 단위 나이 계산기 완성  - DatePickerDialog 및 OnDateSetListener 추가  - 선택 날짜 반영과 SimpleDateFormat 및 날짜 확인

--- app/src/main/java/com/udemy/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/udemy/ageinminutes/MainActivity.kt
@@ -1,16 +1,61 @@
 package com.udemy.ageinminutes
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.udemy.ageinminutes.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        clickDatePicker()
     }
+
+    private fun clickDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this, { _, selectedYear, selectedMonth, selectedDay ->
+                Toast.makeText(
+                    this,
+                    "Year was $selectedYear, Month was ${selectedMonth + 1}, Day was $selectedDay",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                binding.txtDate.text = selectedDate
+
+                val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val date = simpleDateFormat.parse(selectedDate)
+
+                date?.let {
+                    val selectedDateInMinutes = date.time / 60000
+                    val currentDate = simpleDateFormat.parse(simpleDateFormat.format(System.currentTimeMillis()))
+
+                    currentDate?.let {
+                        val currentDateInMinutes = currentDate.time / 60000
+                        val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+                        binding.txtMinutesDate.text = differenceInMinutes.toString()
+                    }
+                }
+            }, year, month, day
+        )
+
+        binding.btnDatePicker.setOnClickListener {
+            datePickerDialog.datePicker.maxDate = System.currentTimeMillis() - 86400000
+            datePickerDialog.show()
+        }
+    }
+
 }
